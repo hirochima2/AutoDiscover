fb40c306e8f02dc41817321a4ad2f76d5fac5901
hirochima2 <99726382+hirochima2@users.noreply.github.com>
2025-01-08 00:35:59 +0100

    Update CarList.js

--- client/src/components/CarList.js
+++ client/src/components/CarList.js
@@ -1,1 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
+const CarList = () => {
+  const [cars, setCars] = useState([]);
+
+  useEffect(() => {
+    const fetchCars = async () => {
+      const response = await axios.get('https://your-backend-url/api/cars');
+      setCars(response.data);
+    };
+    fetchCars();
+  }, []);
+
+  return (
+    <div>
+      <h2>Available Cars</h2>
+      <ul>
+        {cars.map(car => (
+          <li key={car._id}>
+            <Link to={`/car/${car._id}`}>{car.brand} {car.model}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default CarList;
