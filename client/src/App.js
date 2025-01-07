7dcd9aea5fcbe2e81e724fa006951f2ba4c55775
hirochima2 <99726382+hirochima2@users.noreply.github.com>
2025-01-08 00:31:31 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,1 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import CarList from './components/CarList';
+import CarDetail from './components/CarDetail';
 
+function App() {
+  return (
+    <Router>
+      <div>
+        <h1>AutoDiscover: Explore, Compare and Review Cars</h1>
+        <Switch>
+          <Route path="/" exact component={CarList} />
+          <Route path="/car/:id" component={CarDetail} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
