c1019b5f929c549e887e4ed3b2ad71e6e3e7a6c9
hirochima2 <99726382+hirochima2@users.noreply.github.com>
2025-01-08 01:39:36 +0100

    Update CarList.js

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,1 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
+const CarList = () => {
+  const [cars, setCars] = useState([]);
+
+  useEffect(() => {
+    const fetchCars = async () => {
+      const response = await axios.get('https://autodiscover-d1ql.onrender.com/api/cars');
+      setCars(response.data);
+    };
+    fetchCars();
+  }, []);
+
+  return (
+    <div>
+      <h2>Available Cars</h2>
+      <ul>
+        {cars.map(car => (
+          <li key={car.id}>{car.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default CarList;
