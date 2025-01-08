25b74439a1134444e73cd8e24b494e8a8d2891c3
hirochima2 <99726382+hirochima2@users.noreply.github.com>
2025-01-08 01:53:31 +0100

    Update CarDetail.js

--- src/components/CarDetail.js
+++ src/components/CarDetail.js
@@ -8,4 +8,25 @@ const CarDetail = ({ match }) => {
 
   useEffect(() => {
     const fetchCar = async () => {
-      const response = await axios.get
+      const response = await axios.get (`https://autodiscover-server.onrender.com/api/cars/${match.params.id}`);
+      setCar(response.data);
+      setLoading(false);
+    };
+    fetchCar();
+  }, [match.params.id]);
+
+  if (loading) return <div>Loading...</div>;
+
+  return (
+    <div>
+      <h2>{car.name}</h2>
+      <p>Engine Type: {car.engineType}</p>
+      <p>Horsepower: {car.horsepower}</p>
+      <p>Fuel Efficiency: {car.fuelEfficiency}</p>
+      <p>Dimensions: {car.dimensions}</p>
+      <ReviewForm carId={car.id} />
+    </div>
+  );
+};
+
+export default CarDetail;
