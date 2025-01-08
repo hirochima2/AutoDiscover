87fb5c11c8355e70b2db288ef80962961e2a1a55
hirochima2 <99726382+hirochima2@users.noreply.github.com>
2025-01-08 01:39:22 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import CarList from './components/CarList';
 
+function App() {
+  return (
+    <div className="App">
+      <h1>AutoDiscover: Explore, Compare and Review Cars</h1>
+      <CarList />
+    </div>
+  );
+}
+
+export default App;
