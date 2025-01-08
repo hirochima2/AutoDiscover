75687689cae9b65876f610ee8ef5e437c79a4cb6
hirochima2 <99726382+hirochima2@users.noreply.github.com>
2025-01-08 01:39:48 +0100

    Update CarDetail.js

--- src/components/CarDetail.js
+++ src/components/CarDetail.js
@@ -1,1 +1,13 @@
+import React from 'react';
 
+const CarDetail = ({ car }) => {
+  return (
+    <div>
+      <h3>{car.name}</h3>
+      <p>Specifications: {car.specifications}</p>
+      {/* Add more details as needed */}
+    </div>
+  );
+};
+
+export default CarDetail;
